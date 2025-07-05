ab6753ea80aa75bbd4f0aebf91548e2395caf69e
Gowthami1204 <160033319+Gowthami1204@users.noreply.github.com>
2025-07-05 14:28:19 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+ef factorial(n):
+    """Calculate the factorial of a non-negative integer n recursively."""
+    if n < 0:
+        raise ValueError("Factorial is not defined for negative numbers.")
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
